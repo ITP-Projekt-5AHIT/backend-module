b277617a33eaf244f49a007730197fd9ae308c7d
Paul Seidermann <77615910+TheConsoleLog@users.noreply.github.com>
2024-12-08 00:03:26 +0100

    feat: added date constraint to tour schema

--- src/schema/tour/index.ts
+++ src/schema/tour/index.ts
@@ -1,8 +1,10 @@
+import dayjs from "dayjs";
 import { coerce, object, string } from "zod";
 
 export const subscribeSchema = object({
   body: object({
-    accessCode: coerce.number({ message: "Der Beitrittscode muss mitgesendet werden" })
+    accessCode: coerce
+      .number({ message: "Der Beitrittscode muss mitgesendet werden" })
       .min(100_000_000, { message: "Der Beitrittscode muss 9 stellig sein" })
       .max(999_999_999, { message: "Der Beitrittscode muss 9 stellig sein" }),
   }),
@@ -15,12 +17,20 @@ export const tourSchema = object({
         message: "Der Name muss mindestens 5 Zeichen lang sein",
       })
       .max(100, { message: "Der Name darf maximal 100 Zeichen lang sein" }),
-    startDate: coerce.date({
-      message: "Ein Startdatum muss festgelegt werden",
-    }),
-    endDate: coerce.date({
-      message: "Ein Enddatum muss festgelegt werden",
-    }),
+    startDate: coerce
+      .date({
+        message: "Ein Startdatum muss festgelegt werden",
+      })
+      .refine((date) => dayjs().isBefore(date), {
+        message: "Die Tour kann nicht in der Vergangenheit anfangen",
+      }),
+    endDate: coerce
+      .date({
+        message: "Ein Enddatum muss festgelegt werden",
+      })
+      .refine((date) => dayjs().isBefore(date), {
+        message: "Die Tour kann nicht in der Vergangenheit aufhören",
+      }),
     description: string({
       message: "Die Tour muss eine Beschreibung besitzen",
     })
@@ -30,5 +40,12 @@ export const tourSchema = object({
       .max(10000, {
         message: "Die Beschreibung darf maximal 10.000 Zeichen lang sein",
       }),
-  }),
+  }).refine(
+    (tour) => {
+      const begin = dayjs(tour.startDate);
+      const end = dayjs(tour.endDate);
+      return begin.isBefore(end) && end.diff(begin, "hours") >= 1;
+    },
+    { message: "Die Tour muss mindestens 1 Stunde dauern" }
+  ),
 });
