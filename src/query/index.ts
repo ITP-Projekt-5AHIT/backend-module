840424f9494bb1f0450f2df2df62bdc94588de3c
LockenPaul <77615910+TheConsoleLog@users.noreply.github.com>
2025-01-13 10:11:09 +0100

    feat: query structure

---
+++ src/query/index.ts
@@ -0,0 +1,9 @@
+import * as checkpointQuerys from "./checkpoint.query";
+import * as tourQuerys from "./tour.query";
+
+const queries = {
+  cp: checkpointQuerys,
+  tour: tourQuerys,
+};
+
+export default queries;
