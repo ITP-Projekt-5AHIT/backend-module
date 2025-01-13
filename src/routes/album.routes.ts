2818ee9ba8f3dc1c2a5a1d06cfd52676b287c20c
Paul Seidermann <77615910+TheConsoleLog@users.noreply.github.com>
2025-01-13 22:20:30 +0100

    fix: validation was missing on album routes

--- src/routes/album.routes.ts
+++ src/routes/album.routes.ts
@@ -1,10 +1,10 @@
 import { Router } from "express";
 import { validate } from "../middlewares/validation";
 import controllers from "../controllers";
-import { postImageSchema } from "../schema/album.schema";
+import { getAlbumSchema, postImageSchema } from "../schema/album.schema";
 
 const router = Router();
 export default router;
 
 router.post("/", [validate(postImageSchema)], controllers.album.postAddImage);
-router.get("/:alId", controllers.album.getAlbum);
+router.get("/:alId", [validate(getAlbumSchema)], controllers.album.getAlbum);
