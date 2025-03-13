1341610b2c4454ddb02ab82a46dfad0c83cbdc2c
LockenPaul <77615910+TheConsoleLog@users.noreply.github.com>
2025-03-13 13:56:33 +0100

    chore: remove query tools

--- src/utils/queryTools.ts
+++
@@ -1,6 +0,0 @@
-import { PrismaPromise } from "@prisma/client/runtime/library";
-import db from "./db";
-
-const runTransact = async (...promise: PrismaPromise<any>[]) => {
-  return await db.$transaction(promise);
-};
